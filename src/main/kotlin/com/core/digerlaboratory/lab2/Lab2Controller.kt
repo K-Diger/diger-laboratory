01264c2efcaee707a9054bc0f16ae924ddbbe73f
Diger <kitten.diger@gmail.com>
2023-09-01 15:25:11 +0900

    lab2 : @Async 애노테이션

--- src/main/kotlin/com/core/digerlaboratory/lab2/Lab2Controller.kt
+++ src/main/kotlin/com/core/digerlaboratory/lab2/Lab2Controller.kt
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/lab2")
-class TestController2(
-    private val firstLabService: FirstLabService,
+class Lab2Controller(
+    private val lab2Service: Lab2Service,
 ) {
 
     @GetMapping
@@ -15,7 +15,7 @@ class TestController2(
         repeat(10) {
             println("저는 컨트롤러에서 로깅되고 있어요 - ${it}")
         }
-        firstLabService.asyncMethod()
+        lab2Service.asyncMethod()
         repeat(10) {
             println("저는 컨트롤러에서 비동기 메서드를 호출하고 로깅되고 있어요 - ${it}")
         }
--- src/main/kotlin/com/core/digerlaboratory/lab2/Lab2Service.kt
+++ src/main/kotlin/com/core/digerlaboratory/lab2/Lab2Service.kt
@@ -4,7 +4,7 @@ import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 
 @Service
-class FirstLabService {
+class Lab2Service {
 
     @Async
     @Throws(InterruptedException::class)
